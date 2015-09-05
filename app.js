4639632d31570cbfc1d0cecc326f9c1b6ae646f4
josephbrockw <joew@theroasterie.com>
2015-09-05 11:23:40 -0500

    functional drawing board

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 $('document').ready(function() {
-
+	// var i=1;
+	// Start Drawing button functionality
 	$('.startButton').mouseup(function() {
 		$('.home').hide();
 		$('.board').css('display','initial');
+		createBoard();
 	});
 
+	// Create drawing board
+	function createBoard() {
+		
+		for (i = 0; i < 1024; i++) {
+			$(".board").append("<div class=\"square\"></div>");
+		}
+
+		//Drawing Function
+		$('.square').mouseenter(function() {
+			$(this).css('background-color', 'black');
+			console.log('success');
+		})
+	}
+
+	// Drawing function
+	// $('.square').mouseenter(fucntion() {
+	// 	// $(this).css('background-color', 'black');
+	// 	console.log('success');
+	// });
+
+	
 });
